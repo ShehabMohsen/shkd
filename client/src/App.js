5c725ba09f61013a70d71f3ed1b3299edb304e82
Hyemin.Shin <sshin.hyemin@gmail.com>
2022-12-03 16:56:28 -0500

    login/register page + link on landing page updated

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import {
 import Navigation from './components/Navigation';
 import LandingPage from './pages/LandingPage';
 import ListingsPage from './pages/ListingsPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
       <Routes>
         <Route path="/" element = {<LandingPage/>} />
         <Route path="/listings" element = {<ListingsPage/>} />
+        <Route path="/register" element = {<RegisterPage/>} />
+        <Route path="/login" element = {<LoginPage/>} />
       </Routes>
       </BrowserRouter>
     </ChakraProvider>
