fa38abc2b9877ad10eb75bad3c7cf6adaacefbb5
ShehabMohsen <aquashehab612@gmail.com>
2022-12-09 18:00:47 -0500

    createListing route

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ function App() {
                 <Route path="/listings" element={<ListingsPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="listing/create" element={CreateListing} />
+                <Route path="listing/create" element={<CreateListing/>}/>
               </Routes>
             </BrowserRouter>
           </ListingContextProvider>
