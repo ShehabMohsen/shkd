fdba9472e54bb75840b99b3a48dc07747e8998e4
dwang <dwang.ccny@gmail.com>
2022-12-09 22:55:10 -0500

    Establishing skeleton for order's two route

--- api/controllers/order.js
+++ api/controllers/order.js
@@ -7,9 +7,15 @@ const { Listing } = db;
 
 // The purpose of this router is to store order transactions
 
-router.get("/", (req, res) => {
+//Getting all the order for a user
+router.get("/", passport.isAuthenticated(), (req, res) => {
     Listing.findAll({}).then((allListing) => res.json(allListing));
 });
 
+
+//Adding a new order transaction
+router.post("/createOrder", passport.isAuthenticated(), (req,res) =>{
+
+});
 
 module.exports = router
