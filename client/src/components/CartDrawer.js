661273a58a15a095b8545b9ed2c46d274e12c8f1
ShehabMohsen <aquashehab612@gmail.com>
2022-12-10 03:57:38 -0500

    add/remove from cart table + styling

--- client/src/components/CartDrawer.js
+++ client/src/components/CartDrawer.js
@@ -17,6 +17,9 @@ import {
   VStack,
   Checkbox,
   Box,
+  HStack,
+  Image,
+  useColorModeValue
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { FiShoppingCart } from 'react-icons/fi';
@@ -24,7 +27,8 @@ import { useCartContext } from '../contexts/CartContext';
 import { useState } from 'react';
 import CartTable from './CartTable';
 import Headline from './Headline';
-import { Head } from 'react-axios';
+import Logo from '../Assets/Logo.png';
+
 export default function CartDrawer() {
   // Required to make drawer work
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -32,8 +36,10 @@ export default function CartDrawer() {
   // CartContext
   const { cartVariables } = useCartContext();
   const shoppingCart = cartVariables.shoppingCart;
-
   const [checkoutForm, setCheckoutForm] = useState({});
+  // Drawer background color:  useColorModeValue('lightModeColor', 'darkModeColor')
+  const bg = useColorModeValue('gray.100', 'gray.800')
+  
 
   useEffect(() => {
     // this will prepare checkout form
@@ -49,7 +55,6 @@ export default function CartDrawer() {
     }
   }, []);
 
-  console.log('checkoutForm', checkoutForm);
   return (
     <>
       <Button
@@ -72,9 +77,10 @@ export default function CartDrawer() {
         onClose={onClose}
         finalFocusRef={btnRef}
         size={'lg'}
+        
       >
         <DrawerOverlay />
-        <DrawerContent>
+        <DrawerContent bgColor={bg}>
           <DrawerCloseButton />
           {shoppingCart.length > 0 ? (
             <CartContent shoppingCart={shoppingCart} onClose={onClose} />
@@ -108,19 +114,36 @@ export default function CartDrawer() {
 }
 
 export function CartContent({ shoppingCart, onClose }) {
+  const [isChecked, setIsChecked] = useState(false);
+  const [isButtonLoading, setIsButtonLoading] = useState(false);
+
+  console.log(isChecked)
+
+  function checkoutCart(){
+    if (!isChecked) return
+    setIsButtonLoading(true)
+  }
   return (
     <React.Fragment>
-      <DrawerHeader>Your Shopping Cart</DrawerHeader>
+      <DrawerHeader textColor={'orange.400'}>
+        <HStack>
+          <Image src={Logo} width="45px" marginLeft="2px" />
+          <Text>Your Shopping Cart</Text>
+        </HStack>
+      </DrawerHeader>
+
       <DrawerBody>
         <CartTable shoppingCart={shoppingCart} />
-      </DrawerBody>  
-          <Checkbox ml={6} defaultChecked>
-            By checking this box, you are confirming to Checkout
-          </Checkbox>
+      </DrawerBody>
+      <HStack>
+      <Checkbox ml={6} required onChange={()=>{setIsChecked(!isChecked)}}>
+        <Text>By checking this box, you are confirming to Checkout</Text>
+      </Checkbox>
+        </HStack>
       <DrawerFooter>
-          <Button colorScheme="blue">Checkout</Button>
+        <Button size={'md'}colorScheme="blue" isLoading = {isButtonLoading} onClick={checkoutCart}>Checkout</Button>
         <Spacer />
-        <Button variant="outline" ml={3} onClick={onClose}>
+        <Button size = {'md'} variant="outline" ml={3} onClick={onClose}>
           Cancel
         </Button>
       </DrawerFooter>
--- client/src/components/CartTable.js
+++ client/src/components/CartTable.js
@@ -9,38 +9,75 @@ import {
   Td,
   TableCaption,
   TableContainer,
+  VStack,
+  HStack,
+  Text,
+  IconButton,
+  Spacer,
+  Box,
 } from '@chakra-ui/react';
+import { AddIcon, MinusIcon } from '@chakra-ui/icons';
+import { useCartContext } from '../contexts/CartContext';
+
 
 export default function CartTable({ shoppingCart }) {
-  let totalPrice = 0
+
+  //contains states/functions relating to cart such as shoppingCart, setShoppingCart, addToCart, removeFromCart, etc
+  const { cartVariables } = useCartContext();
+  
+  // handler for decrementing item quantity from within cart table
+  function onClickRemove(listing){
+    cartVariables.removeFromCart(listing)
+  }
+
+  // handler for incrementing item quantity from within cart table
+  function onClickAdd(listing){
+    cartVariables.addToCart(listing)    
+  }
+
+  let totalPrice = 0;
   return (
     <TableContainer>
       <Table variant="striped" colorScheme="blue">
         <Thead>
           <Tr>
             <Th>Listing Name</Th>
+            <Th>Quantity</Th>
             <Th>Size</Th>
             <Th isNumeric>Price</Th>
           </Tr>
         </Thead>
         {/* map over shoppingCart in Tbody */}
         <Tbody>
-          {shoppingCart.map((listing) => {
-            totalPrice += listing.price*listing.quantity
+          {shoppingCart.map(listing => {
+            totalPrice += listing.price * listing.quantity;
             return (
               <Tr>
-                <Td>{listing.listing_name}</Td>
+                <Td>
+                  <Text mb={3}>{listing.listing_name}</Text>
+                </Td>
+                <Td>
+                  {/* display quantity of item as well as buttons to add/subtract quantity */}
+                  <HStack spacing={3}>
+                    <IconButton size={'sm'} icon={<MinusIcon />} onClick={()=>{onClickRemove(listing)}} />{' '}
+                    <Text fontSize={18}>{listing.quantity}</Text>
+                    <IconButton size={'sm'} icon={<AddIcon/>} onClick={()=>{onClickAdd(listing)}} />
+                  </HStack>
+                </Td>
                 <Td>{listing.size}</Td>
-                <Td isNumeric>${listing.price*listing.quantity} </Td>
+                <Td isNumeric>${listing.price * listing.quantity} </Td>
               </Tr>
             );
           })}
         </Tbody>
-        <Tfoot >
+        <Tfoot>
           <Tr>
             <Th fontSize={'md'}>Total Price</Th>
             <Th></Th>
-            <Th isNumeric  fontSize={'md'}>${totalPrice}</Th>
+            <Th></Th>
+            <Th isNumeric fontSize={'md'}>
+              ${totalPrice}
+            </Th>
           </Tr>
         </Tfoot>
       </Table>
