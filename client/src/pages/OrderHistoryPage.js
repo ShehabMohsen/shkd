802039d540527374bb66e38903823eda8ee18a7d
dwang <dwang.ccny@gmail.com>
2022-12-11 03:12:35 -0500

    Order Table Heading

--- client/src/pages/OrderHistoryPage.js
+++ client/src/pages/OrderHistoryPage.js
@@ -15,26 +15,26 @@ import {
     Spacer,
     Box,
     Center,
-  } from '@chakra-ui/react'
+} from '@chakra-ui/react'
 import React, { useState, useEffect } from 'react';
 import CartTable from '../components/CartTable';
-export default function OrderHistoryPage(){
+export default function OrderHistoryPage() {
     const [isLoading, setIsLoading] = useState(true);
     const [orders, setOrders] = useState({});
 
     //Fetches Order Data
-    useEffect(() =>{
-        async function getOrderHistory(){
-            try{
+    useEffect(() => {
+        async function getOrderHistory() {
+            try {
                 let response = await fetch(`/api/order/myOrders`)
 
                 if (!response.ok) throw new Error('Unable to get orders');
 
                 let fetchedOrders = await response.json();
                 setOrders(fetchedOrders);
-                
+
                 setIsLoading(false);
-            } catch (err){
+            } catch (err) {
                 console.log(err);
             }
         }
@@ -44,19 +44,24 @@ export default function OrderHistoryPage(){
     //Generate Order History Page
     return (
         <React.Fragment>
-            <Spacer h = '30px'/>
-            <Heading textAlign = "center">Your Order History</Heading>
-            <Spacer h = '30px'/>
-                {isLoading ? null : orders.map((order,index) => {
-                    return (
-                    <Flex justify = "center" >
-                        <Box width = 'xl' mb = '20' >
-                            <CartTable key = { index } shoppingCart={ order.listings} isPrevOrder = { true }/>
+            <Spacer h='30px' />
+            <Heading textAlign="center">Your Order History</Heading>
+            <Spacer h='30px' />
+            {isLoading ? null : orders.map((order, index) => {
+                return (
+                    <Flex justify="center" >
+                        <Box width='xl' mb='20' >
+                            <HStack>
+                                <Heading size="md">Order #{order.id}</Heading>
+                                <Spacer h ='30px'/>
+                                <Heading size = "xs"> {order.createdAt}</Heading>
+                            </HStack>
+                            <CartTable key={index} shoppingCart={order.listings} isPrevOrder={true} />
                         </Box>
                     </Flex>
-                    );
-                })}  
+                );
+            })}
         </React.Fragment>
     )
-    
+
 }
