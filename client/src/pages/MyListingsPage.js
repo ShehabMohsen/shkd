9933a5d365fb8a7cdc8e33c56a19b8a856f3acf3
Shehab Mohsen <aquashehab612@gmail.com>
2022-12-12 10:36:22 -0500

    mylistings page bg

--- client/src/pages/MyListingsPage.js
+++ client/src/pages/MyListingsPage.js
@@ -37,49 +37,49 @@ export default function ListingsPage() {
     getListingData();
   }, []);
 
-  
-
   return (
     <React.Fragment>
-      <Box bg={useColorModeValue('gray.50', 'gray.800')} px={90} py={30}>
-        <Grid
-          h="300px"
-          templateColumns="repeat(4, 1fr)"
-          gap="7"
-          fontWeight="bold"
-        >
-          <GridItem colSpan={'4'} h="40px" >
-            <Stack spacing={4}>
-              <InputGroup>
-                <InputLeftElement
-                  pointerEvents="none"
-                  children={<Search2Icon color="gray.300" />}
-                />
-                <Input type="search" placeholder="Search for item" />
-              </InputGroup>
-            </Stack>
-          </GridItem>
+      <Box bg={useColorModeValue('gray.50', 'gray.800')} h={'90vh'} px={90} py={30}>
+        {!isLoading ? (
+          <>
+            <Grid
+              h="300px"
+              templateColumns="repeat(4, 1fr)"
+              gap="7"
+              fontWeight="bold"
+            >
+              <GridItem colSpan={'4'} h="40px">
+                <Stack spacing={4}>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<Search2Icon color="gray.300" />}
+                    />
+                    <Input type="search" placeholder="Search for item" />
+                  </InputGroup>
+                </Stack>
+              </GridItem>
 
-          {!isLoading ? (
-            listings.map(itemData => {
-              return (
-                <GridItem colSpan={1}>
-                  <ProductCard
-                    key={itemData.id}
-                    imageURL={itemData.image}
-                    name={itemData.listing_name}
-                    price={itemData.price}
-                    gender={itemData.gender}
-                    size={itemData.size}
-                    itemData={itemData}
-                  />
-                </GridItem>
-              );
-            })
-          ) : (
-            <LoadingSpinner />
-          )}
-        </Grid>
+              {listings.map(itemData => {
+                return (
+                  <GridItem colSpan={1}>
+                    <ProductCard
+                      key={itemData.id}
+                      imageURL={itemData.image}
+                      name={itemData.listing_name}
+                      price={itemData.price}
+                      gender={itemData.gender}
+                      size={itemData.size}
+                      itemData={itemData}
+                    />
+                  </GridItem>
+                );
+              })}
+            </Grid>
+          </>
+        ) : (
+          <LoadingSpinner />
+        )}
       </Box>
     </React.Fragment>
   );
