f7394abca0a5d94b7d6213896aec993d0ce9772c
dwang <dwang.ccny@gmail.com>
2022-12-13 04:39:48 -0500

    Date is format to MM/DD/YYYY, reference in comments.

--- client/src/pages/OrderHistoryPage.js
+++ client/src/pages/OrderHistoryPage.js
@@ -22,6 +22,16 @@ export default function OrderHistoryPage() {
     const [isLoading, setIsLoading] = useState(true);
     const [orders, setOrders] = useState({});
 
+    /*Reference Used for DateTime:
+    https://stackoverflow.com/questions/68121540/how-can-i-access-my-json-datetime-value-from-my-react-component
+    https://isotropic.co/how-to-format-a-date-as-dd-mm-yyyy-in-javascript/
+    https://stackoverflow.com/questions/27939773/tolocaledatestring-short-format
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#options*/
+    const getFormattedDate = (dateStr: string) => {
+        const date = new Date(dateStr);
+        return date.toLocaleDateString(); //Returns in local time of machine
+      }
+
     //Fetches Order Data
     useEffect(() => {
         async function getOrderHistory() {
@@ -42,6 +52,8 @@ export default function OrderHistoryPage() {
     }, []);
 
     //Generate Order History Page
+    //Converting ISO8601 to timestamp
+    //https://linuxhint.com/date-toisostring-method-javascript/#:~:text=The%20Date%20toISOString()%20method%20is%20used%20to%20format%20the,string%20value%20to%20the%20caller.
     return (
         <React.Fragment>
             <Spacer h='30px' />
@@ -54,7 +66,7 @@ export default function OrderHistoryPage() {
                             <HStack>
                                 <Heading size="md">Order #{order.id}</Heading>
                                 <Spacer h ='30px'/>
-                                <Heading size = "xs"> {order.createdAt}</Heading>
+                                <Heading size = "xs"> {getFormattedDate(order.createdAt)}</Heading>
                             </HStack>
                             <CartTable key={index} shoppingCart={order.listings} isPrevOrder={true} />
                         </Box>
