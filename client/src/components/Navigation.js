1f24c80b8fe2a01cfb7ab92628f604e4bc3aa1e1
ShehabMohsen <aquashehab612@gmail.com>
2022-12-13 19:50:07 -0500

    nav cluster

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -144,7 +144,7 @@ const DesktopNav = () => {
   const linkColor = useColorModeValue('gray.600', 'gray.200');
   const linkHoverColor = useColorModeValue('gray.800', 'white');
   const popoverContentBgColor = useColorModeValue('white', 'gray.800');
-
+  
   return (
     <Stack direction={'row'} spacing={4}>
       {NAV_ITEMS.map(navItem => (
@@ -294,6 +294,10 @@ const MobileNavItem = ({ label, children, href }) => {
 };
 
 const NAV_ITEMS = [
+  {
+    label: 'All Listings',
+    href: '/listings',
+  },
   {
     label: 'Male Clothing',
     href: '#',
