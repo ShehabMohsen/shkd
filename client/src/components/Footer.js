81bb56c56024b3952cf2151fa8bf2a79af19255a
Hyemin.Shin <sshin.hyemin@gmail.com>
2022-12-13 23:34:31 -0500

    layout of team member's name changed

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,51 +1,19 @@
 import {
   Box,
-  chakra,
   Container,
   Link,
   SimpleGrid,
   Stack,
   Text,
-  VisuallyHidden,
-  Input,
-  IconButton,
   useColorModeValue,
   Image,
   HStack,
-  VStack,
   Spacer,
-  Center,
 } from '@chakra-ui/react';
 import React from 'react';
-import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
-import { BiMailSend } from 'react-icons/bi';
 import Logo from '../Assets/Logo.png';
 import githubLogo from '../Assets/githubLogo.png';
 import OrangeGitHubLogo from '../Assets/OrangeGitHubLogo.png';
-
-const SocialButton = ({ children, label, href }) => {
-  return (
-    <chakra.button
-      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
-      w={8}
-      h={8}
-      cursor={'pointer'}
-      as={'a'}
-      href={href}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
-      _hover={{
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}
-    >
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
-  );
-};
 
 const ListHeader = ({ children }) => {
   return (
@@ -64,7 +32,7 @@ export default function Footer() {
     >
       <Container as={Stack} maxW={'6xl'} py={10}>
         <SimpleGrid
-          templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 2fr' }}
+          templateColumns={{ sm: '1fr', md: '2fr 2fr 2fr' }}
           spacing={8}
         >
           {/* SECTION 1 */}
@@ -72,7 +40,10 @@ export default function Footer() {
             <Box>
               <HStack>
                 <Image src={Logo} width={'36px'} />
-                <Text textColor={useColorModeValue('orange.500', 'orange.400')} fontWeight={'semibold'}>
+                <Text
+                  textColor={useColorModeValue('orange.500', 'orange.400')}
+                  fontWeight={'semibold'}
+                >
                   FASHION 4 YOU!
                 </Text>
               </HStack>
@@ -82,22 +53,26 @@ export default function Footer() {
 
           <Stack align={'center'}>
             <ListHeader>Our Team</ListHeader>
-            <Link href={'https://github.com/ShehabMohsen'}>Shehab Mohsen</Link>
-            <Link href={'https://github.com/dwang312'}>David Wang</Link>
-            
-          </Stack>
-          <Stack align={'flex-start'} mt={6}>
-            <ListHeader>{''}</ListHeader>
-            <Link href={'https://github.com/HyeminShin'}>Hyemin Shin</Link>
-            <Link href={'https://github.com/Kianeefondo'}>Kiane Efondo</Link>
+            <HStack>
+              <Link href={'https://github.com/ShehabMohsen'}>
+                Shehab Mohsen
+              </Link>
+              <Spacer />
+              <Link href={'https://github.com/dwang312'}>David Wang</Link>
+            </HStack>
+            <HStack>
+              <Link href={'https://github.com/Kianeefondo'}>Kiane Efondo</Link>
+              <Spacer /> <Spacer />
+              <Link href={'https://github.com/HyeminShin'}>Hyemin Shin</Link>
+            </HStack>
           </Stack>
 
           {/* SECTION 3 */}
           <Stack align={'center'}>
             <ListHeader>Check us out on GitHub!</ListHeader>
             <Link href={'https://github.com/ShehabMohsen/shkd'}>
-              <Image src={OrangeGitHubLogo} width={10} /> 
-              </Link>
+              <Image src={OrangeGitHubLogo} width={10} />
+            </Link>
           </Stack>
         </SimpleGrid>
       </Container>
